0acaa78fa8ffc6bc0217feb9b471dc504bcc6084
Tim Culverhouse <tim@timculverhouse.com>
2025-04-22 16:23:05 -0500

    build: use addModule instead of createModule

--- build.zig
+++ build.zig
@@ -4,7 +4,7 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const io_mod = b.createModule(.{
+    const io_mod = b.addModule("ourio", .{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
